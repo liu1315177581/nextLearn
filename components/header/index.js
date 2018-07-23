a4c2910724991152df91bdc8659c2bc06fd4a85f
liu1315177581 <1315177581@qq.com>
2018-07-23 23:19:47 +0800

    添加header

--- components/header/index.js
+++ components/header/index.js
@@ -3,13 +3,22 @@ import Link from 'next/link'
 export default class Header extends Component {
     render() {
         return (
-            <div>
-				<Link href="/about">
-					<a href="">about</a>
-				</Link>
-				<Link href="/index">
-					<a href="">index</a>
-				</Link>
+            <div className="header">
+				<div className="container">
+					<div className="w3layouts_logo">
+						<a href="index.html"><h1>New<span>Movies</span></h1></a>
+					</div>
+					<div className="w3_search">
+						<input type="text" name="Search" placeholder="请输入查询的视频" required=""/>
+						<input type="submit" value="Go"/>
+					</div>
+					<div className="w3l_sign_in_register">
+						<ul>
+							
+						</ul>
+					</div>
+					<div className="clearfix"> </div>
+				</div>
 			</div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,18 @@ import getConfig from 'next/config'
 
 class Index extends Component {
     static async getInitialProps(context) {
-    	let res = null;
-    	let api = getConfig().publicRuntimeConfig.api;
-    	if(context.req){
-    		res = await fetch(api+'v2/book/1220562')
-    	}else{
-    		res = await fetch('v2/book/1220562')
-    	}
+        let res = null;
+        let api = getConfig().publicRuntimeConfig.api;
+        if (context.req) {
+            res = await fetch(api + 'v2/book/1220562')
+        } else {
+            res = await fetch('v2/book/1220562')
+        }
         const data = await res.json()
         return {
             shows: data
         }
-	}
+    }
 
     render() {
         return (
